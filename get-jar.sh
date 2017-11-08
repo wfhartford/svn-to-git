347a2492006b7970a19ba4deecae95da88449688
Wesley Hartford <wesley@ze.com>
2017-11-08 10:55:38 -0800

    Get jar by building pcompieta's fork which adds --prefix support

--- get-jar.sh
+++ get-jar.sh
@@ -2,4 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-wget https://bitbucket.org/atlassian/svn-migration-scripts/downloads/svn-migration-scripts.jar
+rm -rf work/svn-migration-scripts svn-migration-scripts.jar
+git clone git@bitbucket.org:pcompieta/svn-migration-scripts.git work/svn-migration-scripts
+(cd work/svn-migration-scripts && sbt -java-home $JAVA_6_HOME proguard)
+ln -s work/svn-migration-scripts/target/svn-migration-scripts.jar .
