330ef217b7c8bc78463b982fdbdce8b43499aa15
Wesley Hartford <wesley@ze.com>
2017-11-17 08:57:13 -0800

    Add log files and preserving repositories not being cloned

--- git-svn-clone.sh
+++ git-svn-clone.sh
@@ -11,15 +11,24 @@ fi
 
 if [ ! -e work/authors.txt ]
 then
-  echo "Authors file must be created prior to repository clonging"
+  echo "Authors file must be created prior to repository cloning"
   exit 2
 fi
 
-rm -rf work/git work/git-bare
-mkdir work/git work/git-bare
+if [ ! -d work/git ]
+then
+  mkdir work/git
+fi
+if [ ! -d work/git-bare ]
+then
+  mkdir work/git-bare
+fi
+if [ ! -d work/logs ]
+then
+  mkdir work/logs
+fi
 
 defaultLayout=$(grep "^,," $layoutsFile)
-echo "Default Layout: $defaultLayout"
 
 originalGcValue=$(git config --global --get gc.auto || echo 'unset')
 git config --global gc.auto 0
@@ -32,6 +41,8 @@ function cleanup {
   fi
 }
 trap cleanup EXIT
+
+time=$(date -u +%Y%m%d%H%M%S)
 
 for repo in work/svn/repos/*
 do
@@ -51,22 +62,32 @@ do
       if [ ! -z "$arguments" ]
       then
         echo "Cloning SVN repo $name to Git repo $gitRepo with arguments $arguments"
-        eval git svn clone --authors-file=work/authors.txt $arguments $url work/git/$gitRepo
+        if [ -d work/git/$gitRepo ]
+        then
+          rm -rf work/git/$gitRepo
+        fi
+        if [ -L work/git-bare/$gitRepo ]
+        then
+          rm work/git-bare/$gitRepo
+        fi
+        eval git svn clone --authors-file=work/authors.txt $arguments $url work/git/$gitRepo \
+            > work/logs/$gitRepo-git-svn-clone-$time.log \
+            2> work/logs/$gitRepo-git-svn-clone-$time.err
         (
           cd work/git/$gitRepo
           if java -Dfile.encoding=utf-8 -jar ../../../svn-migration-scripts.jar clean-git --force --prefix=origin/
           then
-            echo "Unexpected clean-git exit code of 0"
+            echo "Unexpected clean-git exit code of 0" > 2
             exit 1
           else
             exitCode=$?
             if [ ! $exitCode -eq 1 ]
             then
-              echo "Unexpected clean-git exit code of $exitCode"
+              echo "Unexpected clean-git exit code of $exitCode" > 2
               exit 1
             fi
           fi
-        )
+        ) > work/logs/$gitRepo-clean-git-$time.log
         ln -s ../git/$gitRepo/.git work/git-bare/$gitRepo
       fi
     fi
