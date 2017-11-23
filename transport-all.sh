7e08199de56aa1590800eb0406d08a09d0572eb2
Wesley Hartford <wesley@ze.com>
2017-11-23 12:17:36 -0800

    Script to run transport for all rules

---
+++ transport-all.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+limit=$(nproc)
+function limitBg {
+  while [ $(jobs | wc -l) -ge $limit ]
+  do
+    sleep 5
+  done
+}
+
+for rulesFile in rules/*/main.rules
+do
+  svnPath=svn/$(basename $(dirname $rulesFile))
+  if [ -d $svnPath ]
+  then
+    limitBg
+    (
+        ./transport.sh $svnPath $rulesFile
+        echo "Transport of $svnPath compete"
+    ) &
+  else
+    echo "No directory where SVN repository is expected: $svnPath"
+  fi
+done
+
+echo "Waiting for transport operations to complete"
+wait
